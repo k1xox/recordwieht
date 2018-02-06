d9c9002ecb3601fc3fc1511d3023b3ea2d34a420
kamonchanok <primkamonchanok@gmail.com>
2018-02-06 10:35:12 +0700

    Add Value to UserTABLE Success

--- app/src/main/java/kamonchanok/krirk/ac/th/recordwieht/MainActivity.java
+++ app/src/main/java/kamonchanok/krirk/ac/th/recordwieht/MainActivity.java
@@ -3,11 +3,24 @@ package kamonchanok.krirk.ac.th.recordwieht;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kamonchanok.krirk.ac.th.recordwieht.utility.MyManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyManager myManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        myManager = new MyManager(MainActivity.this);
+
+//        Test Add User
+        myManager.addValueTOUserTABLE("nick", "start",
+                "wight", "height", "gender",
+                "age");
+
+
+    }//Main Method
+}//Main Class
--- app/src/main/java/kamonchanok/krirk/ac/th/recordwieht/utility/MyManager.java
+++ app/src/main/java/kamonchanok/krirk/ac/th/recordwieht/utility/MyManager.java
@@ -1,5 +1,6 @@
 package kamonchanok.krirk.ac.th.recordwieht.utility;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -18,7 +19,31 @@ public class MyManager {
 
         myOpenHelperSQLite = new MyOpenHelperSQLite(context);
         sqLiteDatabase = myOpenHelperSQLite.getWritableDatabase();
-        
+
 
     }// Constructor
+
+    //Add Value to userTABLE
+    public long addValueTOUserTABLE(String nickNameString,
+                                    String startAppString,
+                                    String wieghtString,
+                                    String heightString,
+                                    String genderString,
+                                    String ageString) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NickName",nickNameString);
+        contentValues.put("StartApp",startAppString);
+        contentValues.put("Weight",wieghtString);
+        contentValues.put("Height",heightString);
+        contentValues.put("Gender",genderString);
+        contentValues.put("Age",ageString);
+
+
+        return sqLiteDatabase.insert("userTABLE",null,contentValues);
+    }
+
+
+
+
 }// Main Class
