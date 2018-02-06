66d8b4aa94ad825db6d5f4a8ec492d7a5ba2d6fd
kamonchanok <primkamonchanok@gmail.com>
2018-02-06 12:07:31 +0700

    Finish 6Feb

--- app/src/main/java/kamonchanok/krirk/ac/th/recordwieht/MainActivity.java
+++ app/src/main/java/kamonchanok/krirk/ac/th/recordwieht/MainActivity.java
@@ -1,9 +1,11 @@
 package kamonchanok.krirk.ac.th.recordwieht;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kamonchanok.krirk.ac.th.recordwieht.utility.MyManager;
+import kamonchanok.krirk.ac.th.recordwieht.utility.MyOpenHelperSQLite;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
         myManager = new MyManager(MainActivity.this);
 
 //        Test Add User
-        myManager.addValueTOUserTABLE("nick", "start",
-                "wight", "height", "gender",
-                "age");
+//        testAddUser();
+
+//        Delete All userTABlE
+//        deleteAllUserTABlE();
 
 
     }//Main Method
+
+    private void deleteAllUserTABlE() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelperSQLite.databaseName,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete("userTABLE", null, null);
+    }
+
+    private void testAddUser() {
+        myManager.addValueTOUserTABLE("nick", "start",
+                "wight", "height", "gender",
+                "age");
+    }
 }//Main Class
